f2241233c3dcc4d429e1648b63597cdaea04b093
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-03-23 18:29:34 +0100

    feat: create dependency for the project

---
+++ typescript/src/tutorial.ts
@@ -0,0 +1,25 @@
+console.log("hello");
+interface someVal{
+    name: string,
+    age:number
+}
+let someObj: someVal = {
+    name: "motun",
+    age:24
+}
+let awesomeName: string = "motun";
+let awesomeAge: number = 234;
+let awesomeBool :boolean = false
+
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName)
+let amount:number = 40;
+amount = amount -21;
+console.log(amount)
+let greeting: string = "hello Motun";
+greeting = greeting.toUpperCase();
+let age: number = 12;
+let tax: number | string = 10;
+tax = 100;
+tax = "$100"
+
